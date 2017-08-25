a60e2ba79eecee5e464b91a12b3073deb7c7a3d5
Bobby Bryant <bobby@Bobbys-MacBook-Pro.local>
2017-08-25 14:48:47 -0400

    update form functions to work with post object as well as post ids

--- includes/form-functions.php
+++ includes/form-functions.php
@@ -9,16 +9,21 @@ function get_form_by_post_id( $post_id ) {
 	return ( ! empty( $form ) ) ? $form[0] : false;
 }
 
-function get_form_values( $slug, $post ) {
+function get_form_values( $slug, $post, $print_values = false ) {
 	$fields = Core\get_fields( $slug );
+	$post_id = is_object( $post ) ? $post->ID : $post;
 
 	if ( empty( $fields ) || ! is_array( $fields ) ) {
 		return false;
 	}
 
-	return array_reduce( $fields, function( $acc, $field ) use( $post ) {
+	return array_reduce( $fields, function( $acc, $field ) use( $post_id, $print_values ) {
 		$key = sprintf( 'omg-forms-%s', $field[ 'slug' ] );
-		$value = get_post_meta( $post->ID, $key, true );
+		$value = get_post_meta( $post_id, $key, true );
+
+		if ( true === $print_values && is_array( $value ) ) {
+			$value = implode( ',', $value );
+		}
 
 		$acc[ $field[ 'label' ] ] = $value;
 
