d28f9ba27159425be7c95a2b545ec9b90894b00c
Bobby Bryant <bobby@Bobbys-MacBook-Pro-2.local>
2018-04-16 16:07:52 -0400

    upgrade version of omg-fields

--- omg-forms-basic.php
+++ omg-forms-basic.php
@@ -9,7 +9,7 @@ if ( ! defined( 'OMG_FORMS_BASIC_FILE' ) ) {
 }
 
 if ( ! defined( 'OMG_FORMS_BASIC_VERSION' ) ) {
-	define( 'OMG_FORMS_BASIC_VERSION', '0.6.0' );
+	define( 'OMG_FORMS_BASIC_VERSION', '0.7.0' );
 }
 
 require_once OMG_FORMS_BASICS_DIR . '/includes/core.php';
